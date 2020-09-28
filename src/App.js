47d984cf6c03c36ad6ce9095f8905ffc2082a464
zivile.utkeviciute <zivile.utkeviciute@agillic.com>
2020-09-28 20:53:18 +0200

    ImageCropper  skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ImageCropper from './components/ImageCropper'
 import './App.css';
+import couch from './couch.png'
 
 function App() {
   return (
     <div>
-
+      <ImageCropper imageSrc={couch} />
     </div>
   );
 }
